d73657ef39650023b2b7522ac17d6891941a8b84
何中峰 <1446450047@qq.com>
2022-12-13 15:59:58 +0800

    引入gin 并测试

--- main.go
+++ main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/herzorf/go-todo/mysqlConnect"
 	"github.com/herzorf/go-todo/type/mysql"
+	"net/http"
 )
 
 func main() {
 	DB := mysqlConnect.ConnectMysql()
-	rows, _ := DB.Query("select * from  goTodoTest")
-	//循环读取结果
-	var users []mysql.User
-	for rows.Next() {
-		//将每一行的结果都赋值到一个user对象中
-		var user mysql.User
-		err := rows.Scan(&user.Id, &user.Name, &user.Password)
-		if err != nil {
-			fmt.Println("rows fail")
+
+	route := gin.Default()
+	route.GET("/home", func(c *gin.Context) {
+
+		rows, _ := DB.Query("select * from  goTodoTest")
+		//循环读取结果
+		var users []mysql.User
+		for rows.Next() {
+			//将每一行的结果都赋值到一个user对象中
+			var user mysql.User
+			err := rows.Scan(&user.Id, &user.Name, &user.Password)
+			if err != nil {
+				fmt.Println("rows fail")
+			}
+			//将user追加到users的这个数组中
+			users = append(users, user)
 		}
-		//将user追加到users的这个数组中
-		users = append(users, user)
+		fmt.Println(users)
+		marshal, _ := json.Marshal(users)
+
+		c.String(http.StatusOK, string(marshal))
+	})
+
+	err := route.Run()
+	if err != nil {
+		panic("gin 启动失败")
 	}
-	fmt.Println(users)
 }
